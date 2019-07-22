2e2f40f4aa904ffb2131886538eb53439fd58b40
amay16 <amay723@gmail.com>
2019-07-21 21:39:22 -0700

    Updates

---
+++ precache-manifest.9a73b2b3ce9a026e472d5e4349aabd1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4826957a86a1c76d3c549497cc50d622",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "471d978f5c972540712f",
+    "url": "/portfolio/static/css/main.44d0f178.chunk.css"
+  },
+  {
+    "revision": "13f041da1487d9fd67b2",
+    "url": "/portfolio/static/js/2.684b3312.chunk.js"
+  },
+  {
+    "revision": "471d978f5c972540712f",
+    "url": "/portfolio/static/js/main.478df13e.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8102a14f24018da128dd8526936f56e4",
+    "url": "/portfolio/static/media/ChefUpLogo.8102a14f.png"
+  },
+  {
+    "revision": "b2ccd167c908a44e1dd69df79382286a",
+    "url": "/portfolio/static/media/LinkedIn.b2ccd167.ico"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eb52d3f27ca3ad2593d0fcbe17f0353c.js"
+  "/portfolio/precache-manifest.9a73b2b3ce9a026e472d5e4349aabd1c.js"
 );
 
 self.addEventListener('message', (event) => {
